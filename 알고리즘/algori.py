4b2b03d1494bc8a9912774411c6ea058dd705c0a
park0902 <jm050106@naver.com>
2017-05-10 00:31:58 +0900

    1

--- 알고리즘/algori.py
+++ 알고리즘/algori.py
@@ -10,25 +10,36 @@ letter = {
           '111010':'H'
          }
 
-# p = sorted(list(letter.keys()))
-# q = sorted(list(letter.values()))
+p = sorted(list(letter.keys()))
+q = sorted(list(letter.values()))
 
 
 letter_cnt = int(input())*6
 letter_num = input()
 temp =[]
+# temp = ['001111', '000000', '011100']
 start = 0
 finish = 6
+cnt = 0
+result = []
 
 while finish <= letter_cnt:
     temp.append(letter_num[start:finish])
     start += 6
     finish += 6
 
-paper = []
 for key in temp:
     if key in letter.keys():
-        paper.append(letter[key])
+        result.append(letter[key])
+
+    if key not in letter.keys():
+        for i in range(6):
+            for j in range(6):
+                if key[j] == p[i][j]:
+                    cnt += 1
+
+
+
 
 
 
@@ -36,9 +47,10 @@ for key in temp:
 #     if key == p[0]:
 #         print(key)
 
+print(result)
 # print(p)
 # print(d[0])
 # print(temp[0])
-print(paper)
-print(''.join(paper))
+# print(''.join(result))
+# print(''.join(paper))
 # print(''.join(d)[0])
