f449210c0b877f494874cfcc50a3d3106d4b2fed
park0902 <jm050106@naver.com>
2017-05-13 01:06:07 +0900

    1

--- test.py
+++ test.py
@@ -1,70 +1,83 @@
-# temp = []
-#
-# h = int(input())    # 6
-# l = int(input())    # 41
-# i = 1
-#
-# while 3*i >= l:
-#     for i in range(1,l):
-#         temp.append(3*i)
-#
-# print(temp)
+from tkinter import *
+import random
 
-#
-# n=100
-# a=2;b=7
-# print('2', end='')
-# while b<=n:
-#     print(', %d'%b, end='')
-#     a, b = b, a+b
+# Globals
+WIDTH = 800
+HEIGHT = 600
+SEG_SIZE = 20
+IN_GAME = True
 
+def create_block():
+    global BLOCK
+    posx = SEG_SIZE * random.randint(1, (WIDTH-SEG_SIZE) / SEG_SIZE)
+    posy = SEG_SIZE * random.randint(1, (HEIGHT-SEG_SIZE) / SEG_SIZE)
+    BLOCK = c.create_oval(posx, posy, posx+SEG_SIZE, posy+SEG_SIZE, fill='red')
 
+def main():
+    global IN_GAME
+    if IN_GAME:
+        s.move()
+        head_coords = c.coords(s.segments[-1].instance)
+        x1, y1, x2, y2 = head_coords
+        if x2 > WIDTH or x1 < 0 or y2 > HEIGHT:
+            IN_GAME = False
+        elif head_coords == c.coords(BLOCK):
+            s.add_segment()
+            c.delete(BLOCK)
+            create_block()
+        else:
+            for index in range(len(s.segments)-1):
+                if head_coords == c.coords(s.segments[index].instance):
+                    IN_GAME = False
+        root.after(100, main)
+    else:
+        c.create_text(WIDTH/2, HEIGHT/2, text='GAME OVER', font='Arial 20', fill='red')
 
-# temp = []
-# res = int(input())
-# a = int(input())
-# b = int(input())
-# a;b
-# temp.append(a)
-# while b <= res:
-#     temp.append(b)
-#     a, b = b, a+b
-#
-# print(temp)
+class Segment(object):
+    def __init__(self,x,y):
+        self.instance = c.create_rectangle(x, y, x+SEG_SIZE, y+SEG_SIZE, fill='white')
 
+class Snake(object):
+    def __init__(self, segments):
+        self.segments = segments
+        self.mapping = {'Down':(0,1), 'Right':(1,0), 'Up':(0,-1), 'Left':(-1,0)}
+        self.vector = self.mapping['Right']
 
-# temp_a = []
-# temp_b = []
-#
-# res = int(input())
-# a = int(input())
-# b = int(input())
-# a;b
-# temp.append(a)
-# while b <= res:
-#     temp.append(b)
-#     a, b = b, a+b
-#
-# print(temp)
+    def move(self):
+        for index in range(len(self.segments)-1):
+            segment = self.segments[index].instance
+            x1, y1, x2, y2 = c.coords(self.segments[index+1].instance)
+            c.coords(segment, x1, y1, x2, y2)
 
+        x1, y1, x2, y2 = c.coords(self.segments[-2].instance)
+        c.coords(self.segments[-1].instance,
+                 x1+self.vector[0]*SEG_SIZE, y1+self.vector[1]*SEG_SIZE,
+                 x2+self.vector[0]*SEG_SIZE, y2+self.vector[1]*SEG_SIZE)
 
-#
+    def add_segment(self):
+        last_seg = c.coords(self.segments[0].instance)
+        x = last_seg[2] - SEG_SIZE
+        y = last_seg[3] - SEG_SIZE
+        self.segments.insert(0, Segment(x,y))
 
+    def change_direction(self, event):
+        if event.keysym in self.mapping:
+            self.vector = self.mapping[event.keysym]
+            # print(self.vector)
 
+root = Tk()
+root.title('snake')
 
-def word_cnt():
-    word_script = ''     # 입력받은 단어를 담을 변수
-    while word_script != 'end':        # end를 입력하지 않으면 계속 진행
-        word_list=[]                   # 입력된 단어 중 중복을 제거하고 남은 단어를 담을 변수
-        word_script = str(input('단어를 입력하세요!'))
-        if word_script != 'end':        # end가 입력되면 진행하지말고 종료
-            if len(word_script) < 200:  # 단어 길이가 200 이하로 입력이 되면 실행
-                word = word_script.split()      # 입력받은 word_script를 split함수로 구분
-                word_list = list(set(word))     # list(set())함수로 담겨진 단어들 중 중복 제거
-                word_list.sort()               # sort()함수로 정렬
-                for i in word_list:
-                    word_cnt = 0
-                    word_cnt += word.count(i) # 중복 제거한 word_list에서 word에 몇개가 들어가있는지 count
-                    print(i,':',word_cnt)
+c = Canvas(root, width=WIDTH, height=HEIGHT, bg='black')
+c.grid()
+c.focus_set()
 
-print(word_cnt())
+segments = [Segment(SEG_SIZE, SEG_SIZE),
+            Segment(SEG_SIZE*2, SEG_SIZE),
+            Segment(SEG_SIZE*3, SEG_SIZE)]
+
+s = Snake(segments)
+c.bind('<KeyPress>', s.change_direction)
+create_block()
+main()
+root.mainloop()
