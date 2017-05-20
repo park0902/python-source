756d25d6de01af43ba43a2021beb86d07642d7ad
park0902 <jm050106@naver.com>
2017-05-20 22:04:48 +0900

    1

--- 파이썬 포토폴리오/new.py
+++ 파이썬 포토폴리오/new.py
@@ -2,46 +2,134 @@ import urllib.request           # 웹 브라우져에서 html 문서를 얻어�
 from bs4 import BeautifulSoup  # html 문서검색 모듈
 
 
-# import os
-#
-# def get_save_path():
-#     save_path = input("Enter the file name and file location : ")
-#     save_path = save_path.replace("\\", "/")
-#
-#     if not os.path.isdir(os.path.split(save_path)[0]):
-#         os.mkdir(os.path.split(save_path)[0])   # 폴더가 없으면 만드는 작업
-#     return save_path
+import os
 
+def get_save_path():
+    save_path = input("Enter the file name and file location : ")
+    save_path = save_path.replace("\\", "/")
 
+    if not os.path.isdir(os.path.split(save_path)[0]):
+        os.mkdir(os.path.split(save_path)[0])   # 폴더가 없으면 만드는 작업
+    return save_path
+
+p = 0
+q = 0
 def fetch_list_url():
-    # f = open(get_save_path(), 'w', encoding='utf-8')
-    # for i in range(1,13):
-    #     for j in range(1,32):
-            list_url = "https://www.wunderground.com/history/airport/RKSI/2016/1/2/DailyHistory.html"
+    f = open(get_save_path(), 'w', encoding='utf-8')
+    for i in range(1,2):
+        for j in range(1,2):
+            list_url = "https://www.wunderground.com/history/airport/RKSI/2016/"+str(i)+"/"+str(j)+"/DailyHistory.html"
             url = urllib.request.Request(list_url) # url 요청에 따른 http 통신 헤더값을 얻어낸다
             res = urllib.request.urlopen(url).read().decode("utf-8")
 
             soup = BeautifulSoup(res, "html.parser")  # res html 문서를 BeautifulSoup 모듈을 사용해서 검색할수있도록 설정
             soup_temp = soup.find_all('span', class_='wx-value')
-            print(soup_temp)
-            # x = 0
+            soup_metar = soup.find_all('tr', class_='no-metars')
+            # soup_span = soup.find_all('span', class_='wx-data')
+            # print(soup_metar[j].get_text(strip=True, separator=', '))
+            # print(soup_span)
+            # print(soup_temp)
+            x = 0
             # for index in soup_temp:
             #     print(x,index)
             #     x += 1
+
+            date = '2016'+str(i)+str(j)
             soup_mean_temp = soup_temp[0].get_text()
             soup_max_temp = soup_temp[1].get_text()
             soup_min_temp = soup_temp[4].get_text()
+            soup_dew_temp = soup_temp[7].get_text()
             soup_wind = soup_temp[10].get_text()
             soup_max_wind = soup_temp[11].get_text()
             soup_vis = soup_temp[12].get_text()
-    #
-    #         f.write(soup_mean_temp+', ')
-    #         f.write(soup_max_temp+', ')
-    #         f.write(soup_min_temp+', ')
-    #         f.write(soup_wind+', ')
-    #         f.write(soup_max_wind+', ')
-    #         f.write(soup_vis+'\n')
-    #
-    # f.close()
+            s = soup_temp[13].get_text()
+
+
+            # soup_mean_temp = soup_temp[0].get_text()
+            # soup_max_temp = soup_temp[1].get_text()
+            # soup_min_temp = soup_temp[2].get_text()
+            # soup_dew_temp = soup_temp[3].get_text()
+            # soup_wind = soup_temp[6].get_text()
+            # soup_max_wind = soup_temp[7].get_text()
+            # soup_vis = soup_temp[8].get_text()
+            # s = soup_temp[9].get_text()
+            # print(soup_vis,' ',s )
+            f.write(date+', ')
+            f.write(soup_mean_temp+', ')
+            f.write(soup_max_temp+', ')
+            f.write(soup_min_temp+', ')
+            f.write(soup_dew_temp+', ')
+            f.write(soup_wind+', ')
+            f.write(soup_max_wind+', ')
+            f.write(soup_vis+', ')
+            f.write(s+'\n')
+    f.close()
 
 fetch_list_url()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import urllib.request           # 웹 브라우져에서 html 문서를 얻어오기위해 통신하기 위한 모듈
+# from bs4 import BeautifulSoup  # html 문서검색 모듈
+#
+#
+# import os
+#
+# # def get_save_path():
+# #     save_path = input("Enter the file name and file location : ")
+# #     save_path = save_path.replace("\\", "/")
+# #
+# #     if not os.path.isdir(os.path.split(save_path)[0]):
+# #         os.mkdir(os.path.split(save_path)[0])   # 폴더가 없으면 만드는 작업
+# #     return save_path
+#
+#
+# def fetch_list_url():
+#     # f = open(get_save_path(), 'w', encoding='utf-8')
+#     # for i in range(1,13):
+#     # for j in range(1,32):
+#             list_url = "https://www.wunderground.com/history/airport/RKSI/2016/1/4/DailyHistory.html"
+#             url = urllib.request.Request(list_url) # url 요청에 따른 http 통신 헤더값을 얻어낸다
+#             res = urllib.request.urlopen(url).read().decode("utf-8")
+#
+#             soup = BeautifulSoup(res, "html.parser")  # res html 문서를 BeautifulSoup 모듈을 사용해서 검색할수있도록 설정
+#             soup_temp = soup.find_all('span', class_='wx-value')
+#             soup_metar = soup.find_all('tr', class_='no-metars')
+#             # soup_span = soup.find_all('span', class_='wx-data')
+#             # print(soup_metar[j].get_text(strip=True, separator=', '))
+#             # print(soup_span)
+#             # print(soup_temp)
+#             x = 0
+#             for index in soup_temp:
+#                 print(x,index)
+#                 x += 1
+#             soup_mean_temp = soup_temp[0].get_text()
+#             soup_max_temp = soup_temp[1].get_text()
+#             soup_min_temp = soup_temp[4].get_text()
+#             soup_dew_temp = soup_temp[7].get_text()
+#             soup_wind = soup_temp[10].get_text()
+#             soup_max_wind = soup_temp[11].get_text()
+#             soup_vis = soup_temp[12].get_text()
+#             s = soup_temp[13].get_text()
+#     # #
+#     #         f.write(soup_mean_temp+', ')
+#     #         f.write(soup_max_temp+', ')
+#     #         f.write(soup_min_temp+', ')
+#     #         f.write(soup_dew_temp+', ')
+#     #         f.write(soup_wind+', ')
+#     #         f.write(soup_max_wind+', ')
+#     #         f.write(soup_vis+'\n')
+#     #         # f.write(s+'\n')
+#     # f.close()
+#
+# fetch_list_url()
--- 파이썬 포토폴리오/pandas.py
+++ 파이썬 포토폴리오/pandas.py
@@ -1,52 +1,80 @@
 # import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
+# # import numpy as np
+# # import matplotlib.pyplot as plt
 #
-# rawdate = pd.read_csv('d:\data7\\air.txt', names=['Mean', 'Max', 'Min', 'wind', 'maxwind', 'vis'])
-# print(rawdate.head(10))
+# rawdate = pd.read_csv('d:\data7\\final_incheon_airport.csv', names=['Mean', 'Max', 'Min', 'dew','wind', 'maxwind', 'vis'])
+# print(rawdate.head(366))
 # print(rawdate.plot())
 
 
-import urllib.request           # 웹 브라우져에서 html 문서를 얻어오기위해 통신하기 위한 모듈
-from bs4 import BeautifulSoup  # html 문서검색 모듈
+for i in range(1,10):
+    if i == 1:
+        i = '01'
+    print(i)
 
 
-import os
-
-def get_save_path():
-    save_path = input("Enter the file name and file location : ")
-    save_path = save_path.replace("\\", "/")
-
-    if not os.path.isdir(os.path.split(save_path)[0]):
-        os.mkdir(os.path.split(save_path)[0])   # 폴더가 없으면 만드는 작업
-    return save_path
 
 
-def fetch_list_url():
-    f = open(get_save_path(), 'w', encoding='utf-8')
-    # for i in range(1,13):
-    #     for j in range(1,32):
-    list_url = "https://www.wunderground.com/history/airport/RKSI/2016/1/2/DailyHistory.html"
-    url = urllib.request.Request(list_url) # url 요청에 따른 http 통신 헤더값을 얻어낸다
-    res = urllib.request.urlopen(url).read().decode("utf-8")
+# import urllib.request           # 웹 브라우져에서 html 문서를 얻어오기위해 통신하기 위한 모듈
+# from bs4 import BeautifulSoup  # html 문서검색 모듈
+#
+#
+# import os
+#
+# def get_save_path():
+#     save_path = input("Enter the file name and file location : ")
+#     save_path = save_path.replace("\\", "/")
+#
+#     if not os.path.isdir(os.path.split(save_path)[0]):
+#         os.mkdir(os.path.split(save_path)[0])   # 폴더가 없으면 만드는 작업
+#     return save_path
+#
+#
+# def fetch_list_url():
+#     f = open(get_save_path(), 'w', encoding='utf-8')
+#     # for i in range(1,13):
+#     #     for j in range(1,32):
+#     list_url = "https://www.wunderground.com/history/airport/RKSI/2016/1/2/DailyHistory.html"
+#     url = urllib.request.Request(list_url) # url 요청에 따른 http 통신 헤더값을 얻어낸다
+#     res = urllib.request.urlopen(url).read().decode("utf-8")
+#
+#     soup = BeautifulSoup(res, "html.parser")  # res html 문서를 BeautifulSoup 모듈을 사용해서 검색할수있도록 설정
+#     soup_temp = soup.find_all('span', class_='wx-value')
+#             # print(soup_temp)
+#     soup_mean_temp = soup_temp[0].get_text()
+#     soup_max_temp = soup_temp[1].get_text()
+#     soup_min_temp = soup_temp[4].get_text()
+#     soup_wind = soup_temp[10].get_text()
+#     soup_max_wind = soup_temp[11].get_text()
+#     soup_vis = soup_temp[12].get_text()
+#
+#     f.write(soup_mean_temp+', ')
+#     f.write(soup_max_temp+', ')
+#     f.write(soup_min_temp+', ')
+#     f.write(soup_wind+', ')
+#     f.write(soup_max_wind+', ')
+#     f.write(soup_vis+'\n')
+#
+#     f.close()
+#
+# fetch_list_url()
 
-    soup = BeautifulSoup(res, "html.parser")  # res html 문서를 BeautifulSoup 모듈을 사용해서 검색할수있도록 설정
-    soup_temp = soup.find_all('span', class_='wx-value')
-            # print(soup_temp)
-    soup_mean_temp = soup_temp[0].get_text()
-    soup_max_temp = soup_temp[1].get_text()
-    soup_min_temp = soup_temp[4].get_text()
-    soup_wind = soup_temp[10].get_text()
-    soup_max_wind = soup_temp[11].get_text()
-    soup_vis = soup_temp[12].get_text()
 
-    f.write(soup_mean_temp+', ')
-    f.write(soup_max_temp+', ')
-    f.write(soup_min_temp+', ')
-    f.write(soup_wind+', ')
-    f.write(soup_max_wind+', ')
-    f.write(soup_vis+'\n')
 
-    f.close()
 
-fetch_list_url()
+# import pandas as pd
+# import numpy as np
+#
+# df = pd.read_csv("d:\data7\\final_incheon_airport.csv")
+#
+# df.colums = ['date','a','b','c','d','e','f']
+#
+# df['datetime'] = df['date'].apply(lambda x: pd.to_datetime(str(x), format='%Y%m%d'))
+# df['all_news_num'] = 1
+# print(df.index)
+
+
+
+
+
+
