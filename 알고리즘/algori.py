bdb49c60b4e16dee4e8f06a5da3d609f46120bee
park0902 <jm050106@naver.com>
2017-06-10 17:24:28 +0900

    1

--- 알고리즘/algori.py
+++ 알고리즘/algori.py
@@ -1,34 +1,22 @@
-# list = [1]
-#
-# for i in range(1,1501):
-#     if i%2 == 0 or i%3 == 0 or i%5 == 0:
-#         list.append(i)
-#
-# print(list)
-
-
-
-
 def ugly(n):
     result = [1]
     while True:
         last = result[-1]
-        if len(result)==n:
+
+        if len(result) == n:
             return result
 
-        tmp = []
+        temp = []
+
         for r in result:
+            print(r)
+            print('======')
             for t in r*2,r*3,r*5:
                 if t > last:
-                    tmp.append(t)
-
-        result.append(min(tmp))
-
-
-print(ugly(1500))
+                    temp.append(t)
+                    print(temp)
+                    print('------')
+        result.append(min(temp))
 
 
-
-list = [1,2,3,4]
-
-print(min(list))
+print(ugly(5))
