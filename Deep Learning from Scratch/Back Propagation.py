0e0d13ced260c04e5c3ea344b62d3a16db08151d
park0902 <jm050106@naver.com>
2017-07-04 20:53:18 +0900

    밑바닥부터 시작하는 딥러닝

--- Deep Learning from Scratch/Back Propagation.py
+++ Deep Learning from Scratch/Back Propagation.py
@@ -210,4 +210,88 @@ class Sigmoid:
 
 
 
+'''
+--------------------------------------------------------------------------------------
+- 배치용 Affine 계층
+
+    신경망의 순전파 때 수행하는 행렬의 내적은 기하학에서는 어파인 변환이라고 한다!!
+    
+    어파인 변환을 수행하는 처리를 Affine 계층이라는 이름으로 구현
+--------------------------------------------------------------------------------------
+'''
+
+import numpy as np
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.W.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+
+'''
+--------------------------------------------------------------------------------------
+- Softmax-with-Loss 계층
+
+    마지막 출력층에서 사용하는 소프트맥스 함수
+    소프트맥스 함수는 입력 값을 정규화하여 출력!
+    
+    신경망에서 수행하는 작업은 학습과 추론!
+    
+        추론할 때는 Softmax 계층 사용 X
+        
+        신경망은 추론할 때는 마지막 Affine 계층의 출력을 인식결과로 이용
+        신경망에서 정규화하지 않은 출력결과를 점수(score) 라고 한다!!
+        
+        즉, 신경망 추론애소 답을 하나만 내는 경우에는 가장 높은 점수만 알면 되니 Softmax 계층은 필요X
+        
+    
+        학습할 때는 Softmax 계층 필요!!
+--------------------------------------------------------------------------------------
+'''
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None    # 손실
+        self.y = None       # softmax의 출력
+        self.x = None       # 정답 레이블(원-핫 벡터)
+
+
+    def cross_entropy_error(self, y, t):
+        if y.ndim == 1:
+            self.t = t.reshape(1, t.size)
+            self.y = y.reshape(1, y.size)
+
+        # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+        if t.size == y.size:
+            self.t = t.argmax(axis=1)
+
+        batch_size = y.shape[0]
+
+
+        return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
+    def forward(self, x,t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
 
