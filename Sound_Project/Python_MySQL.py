efdf859f18a52648b964b8eb83e4bbcd67e98c9e
park0902 <jm05010@naver.com>
2018-01-05 19:08:53 +0900

    medici

--- Sound_Project/Python_MySQL.py
+++ Sound_Project/Python_MySQL.py
@@ -4,15 +4,18 @@ import pymysql
 db = pymysql.connect(host='localhost', port=3306, user='root', passwd='!a1s2d3f4', db='CCD', charset='utf8')
 
 # Connection 으로부터 Cursor 생성
-cursor = db.cursor()
+cursor = db.cursor(pymysql.cursors.DictCursor)
 
 # SQL문 실행
-sql = "select * from SOUND"
-cursor.execute(sql)
+sql = "select * from SOUND where SOUND_KEY = %s"
+cursor.execute(sql, ('abc-01'))
 
 # 데이터 Fetch
-data = cursor.fetchall()
-print(data)
+datas = cursor.fetchall()
+
+for data in datas:
+    print(data)
+    print(data['NO'], data['AVG'], data['SOUND_KEY'])
 
 # Connection 닫기
 db.close()
